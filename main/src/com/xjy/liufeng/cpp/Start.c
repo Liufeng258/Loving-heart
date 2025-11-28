a5cc47115c36cf1153898768467defecee187eb5
Liufeng258 <160316559+Liufeng258@users.noreply.github.com>
2025-11-29 07:48:21 +0800

    Add Start.c to display a pattern and 'Hello world!'

---
+++ main/src/com/xjy/liufeng/cpp/Start.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+//#include<windows.h>
+
+int main()
+{
+system("color 0c");
+//double x, y;
+for(double y=1.5;y>=-0.5;y-=0.05)
+{
+for(double x=-1.5;x<=1.5;x+=0.025)
+{
+putchar((pow(x*x+y*y-1,3)<=x*x*pow(y,3))?'*':' ');
+}
+printf("\n");
+//Sleep(100);
+}
+    printf("\tHello world!\n");
+    return 0;
+}
