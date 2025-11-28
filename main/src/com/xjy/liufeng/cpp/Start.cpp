6835dc5ec2c2c508fdc2b9f10d86119572124952
Liufeng258 <160316559+Liufeng258@users.noreply.github.com>
2025-11-29 07:36:36 +0800

    Add C++ program to display pattern and greeting
    Implement a simple C++ program to display a pattern and print 'Hello, World!'

---
+++ main/src/com/xjy/liufeng/cpp/Start.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char** argv) {
+    //system("color 0c");
+    for (double y = 1.4; y >= -1.4; y -= 0.1) {
+        for (double x = -1.4; x <= 1.4; x += 0.05) {
+            cout << /*putchar*/((pow(x*x+y*y-1,3)<=x*x*pow(y,3))?'x':' ');
+        }
+        endl(cout);//cout << '\n'; or cout << endl;
+        //sleep(t); t = 100
+    }
+    cout << "\tHello, World!" << endl;
+    return 0;
+}
