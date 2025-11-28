fc1f2b7af4aa8eef2bc4a137b4ae8669f594787e
Liufeng258 <160316559+Liufeng258@users.noreply.github.com>
2025-11-29 07:38:21 +0800

    Fix formatting of endl usage in Start.cpp

--- main/src/com/xjy/liufeng/cpp/Start.cpp
+++ main/src/com/xjy/liufeng/cpp/Start.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
         for (double x = -1.4; x <= 1.4; x += 0.05) {
             cout << /*putchar*/((pow(x*x+y*y-1,3)<=x*x*pow(y,3))?'x':' ');
         }
-        endl(cout);//cout << '\n'; or cout << endl;
+        endl(cout);//cout << '\n' << endl; or cout << endl;
         //sleep(t); t = 100
     }
     cout << "\tHello, World!" << endl;
