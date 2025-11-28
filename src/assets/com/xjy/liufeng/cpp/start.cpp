0a8d17d78f81d805cec85572497abb5e88d061f9
Liufeng258 <160316559+Liufeng258@users.noreply.github.com>
2025-11-29 06:44:14 +0800

    Implement ASCII art and greeting in start.cpp
    Added a simple graphical representation using ASCII characters and a greeting message.

--- src/assets/com/xjy/liufeng/cpp/start.cpp
+++ src/assets/com/xjy/liufeng/cpp/start.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+//#define RED "\033[31m";
 
+int main(int argc, char** argv) {
+    system("color 0c");
+    for(double i = 1.4; i >= -1.4; i-=0.1) {
+        for(double j = -1.4; j <= 1.4; j+=0.05) {
+            putchar((pow(i*i+j*j-1,3)<=i*i*pow(j,3))?'*':' ');
+        }
+        cout << "\n" << endl;
+        //Sleep(100);
+    }
+
+    cout << "\tHello, World!" << endl;
+    return 0;
+}
